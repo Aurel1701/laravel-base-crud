38c084f907fa0cc87c58b39a869dc585df8898be
Aurel1701 <gantzfan96@gmail.com>
2022-06-16 15:14:42 +0200

    add comics.php in config

---
+++ database/seeds/ComicTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+use App\Comic;
+use Illuminate\Database\Seeder;
+
+class ComicTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comics= config('comics');
+        foreach ($comics as $data) {
+            
+            $comic = new Comic;
+            $comic->title=$data['title'];
+            $comic->thumb=$data['thumb'];
+            $comic->price=$data['price'];
+            $comic->series = $data['series'];
+            $comic->description = $data['description'];
+            $comic->sale_date = $data['sale_date'];
+            $comic->save();
+        }
+
+    }
+}
